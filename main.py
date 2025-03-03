36ff551ad37b94e32077f4fe56ca0fbd2a819a62
Oleh Yemelianov <emelianov2811@gmail.com>
2025-03-03 20:24:29 +0200

    added volume slider support

--- main.py
+++ main.py
@@ -7,50 +7,53 @@ from tkinter import filedialog
 import os
 
 
+pygame.mixer.init()
 sound_pause = False
-sound = None
+sound_stop = False
 file = None
 
 
 def open_file():
-    global file
+    global file, sound_vol
     file = filedialog.askopenfile(mode='r')
 
 def play():
-    global sound, sound_pause
-    pygame.mixer.init()
-    sound = pygame.mixer.Sound(file)
-          
-    if sound_pause:
-        pygame.mixer.unpause()
-        sound_pause = False
-    else:
-        sound.play()
-        
-    if sound is None:
-        label_exc = tk.Label(root, textvariable=val, font=("Arial", 14))
-        label_exc.pack()
-    else:
-        sound.play()
+    global sound_pause
     
+    if file:
+        if sound_pause:
+            pygame.mixer.music.unpause()
+            sound_pause = False
+        else:
+            pygame.mixer.music.load(file)
+            pygame.mixer.music.play()
+    else:
+        label_file = ttk.Label(text='Будь ласка завантажте файл!')
+        label_file.pack()
         
     
-       
+    
+         
     
 def pause():
-    global sound_pause  
-    if sound:
-        pygame.mixer.pause()
+    global sound_pause
+    
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
         sound_pause = True
+    else:
+        label_non_play = ttk.Label(text='Наразі нічого не програється.')
+        label_non_play.pack()
     
     
     
 def stop():
-    sound_stop = pygame.mixer.stop()
+    pygame.mixer.music.stop()
     
 
-def sound_vol(val):
-    mixer.music.set_volume(float(val)/10)
+def sound_vol(volume):
+    mixer.music.set_volume(float(volume)/100)
+    
     
 
 root = Tk()
@@ -71,17 +74,15 @@ btn_stop.place(relx=0.6, rely=0.5, anchor="c", width=80, height=40)
 label_main.pack()
 label_nick.pack()
 
-scal = Scale(root,orient=HORIZONTAL ,length=300,from_=0,to=100,tickinterval=10,resolution=10, command=sound_vol)
+scal = Scale(root,orient=HORIZONTAL ,length=300,from_=0,to=100 ,tickinterval=10,resolution=10, command=sound_vol)
+scal.set(50)
+
 scal.pack(side='left', padx=10)
 scal.pack(side='right', padx=300)
 scal.pack(side='bottom', pady=70)
 
 button_file = Button(text='Вибрати файл', command=open_file)
 button_file.pack()
-
-val = tk.StringVar()
-val.set('Спочатку виберіть файл!')
-    
 
 root.configure(bg='DeepSkyBlue')
 root.mainloop()
